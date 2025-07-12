2f9c47c440913e2439b1a87766c9082c539fc7ca
sagar7378 <sagarprajapat250@gmail.com>
2025-07-12 11:57:38 +0530

    server url change

--- src/config/Api.js
+++ src/config/Api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const token = JSON.parse(localStorage.getItem("user"))?.token;
 
 const Api = axios.create({
-  baseURL: "http://localhost:4001/api/v1/",
+  baseURL: "https://whatsapp-node-z2fc.onrender.com/api/v1/",
   headers: {
     "Content-Type": "application/json",
     Authorization: `Bearer ${token}`,
