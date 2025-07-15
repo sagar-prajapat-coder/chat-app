8292c85dbaac5e8669929dadb803141631762159
Sagar Prajapat <109717363+sagar-prajapat-coder@users.noreply.github.com>
2025-07-15 15:08:31 +0530

    Update Dashboard.jsx

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,21 +1,25 @@
 import axios from "axios";
 import Sidebar from "./layouts/Sidebar";
+import { useEffect, useState } from "react";
 
 function Dashboard() {
-  const userData = JSON.parse(localStorage.getItem("user"))?.data.name;
+  const [userData, setUserData] = useState("");
+  useEffect(() => {
+    const userData = JSON.parse(localStorage.getItem("user"));
+    if (userData?.token) {
+      setUserData(userData);
+    }
+  }, []);
 
   return (
     <div className="flex flex-col md:flex-row h-screen bg-gray-100">
       <div className="w-full md:w-64">
-        <Sidebar />
+        <Sidebar user={userData} />
       </div>
-      {/* Sidebar hidden on small screens, visible on md+ */}
-
-      {/* Main Content */}
       <main className="flex-1 p-4 md:p-6 overflow-y-auto">
         <div className="p-4 bg-white rounded shadow">
           <h2 className="text-base sm:text-lg font-semibold mb-4">
-            Welcome {userData}
+            Welcome {userData?.name}
           </h2>
           <ul className="divide-y divide-gray-100">
             {/* List items go here */}
