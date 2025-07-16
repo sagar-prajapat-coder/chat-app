12fe15181c37f142fa4ca29607b87f80caa66c00
Sagar Prajapat <109717363+sagar-prajapat-coder@users.noreply.github.com>
2025-07-16 17:21:46 +0530

    Update TypingIndicator.jsx

--- src/components/TypingIndicator.jsx
+++ src/components/TypingIndicator.jsx
@@ -2,9 +2,10 @@ import { SyncLoader } from "react-spinners";
 
 export const TypingIndicator = ({ isTyping }) => {
   return (
-    <div className="flex items-end justify-start">
+    <div className="flex items-end justify-start w-full ml-8">
       {isTyping && (
         <SyncLoader
+          color="#16a34a"
           size={10}
           aria-label="Loading Spinner"
           data-testid="loader"
