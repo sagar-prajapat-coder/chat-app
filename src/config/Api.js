4d2c7719a455ebdb5f2057acc0adbd28068b1fe5
Sagar Prajapat <109717363+sagar-prajapat-coder@users.noreply.github.com>
2025-07-16 16:11:41 +0530

    Update Api.js

--- src/config/Api.js
+++ src/config/Api.js
@@ -1,13 +1,26 @@
 import axios from "axios";
 
-const userData = JSON.parse(localStorage.getItem("user"));
+// const userData = JSON.parse(localStorage.getItem("user"));
 
 const Api = axios.create({
   baseURL: "https://whatsapp-node-z2fc.onrender.com/api/v1/",
   headers: {
     "Content-Type": "application/json",
-    Authorization: `Bearer ${userData?.token}`,
+    // Authorization: `Bearer ${userData?.token}`,
   },
 });
 
+
+Api.interceptors.request.use(
+  (config) => {
+    const userData = JSON.parse(localStorage.getItem("user"));
+    if (userData?.token) {
+      config.headers.Authorization = `Bearer ${userData.token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
 export default Api;
+
